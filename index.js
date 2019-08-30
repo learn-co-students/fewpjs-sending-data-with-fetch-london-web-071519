d68dd1dbdec5bca88c3545c8e685809971d0b94c
saazim1123 <saazim1123@gmail.com>
2019-08-30 09:25:03 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+
+// Create Post request to user with a name and email
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }).then(resp => resp.json())
+    .then( function(object) {
+      document.body.innerHTML = object ["id"]
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+}
+
+
+
+// Example
+
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
